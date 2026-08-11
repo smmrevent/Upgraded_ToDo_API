c21c709ab54196fb94ff9c058a75777a3342a3b2
Daniil Trush <mghts11@gmail.com>
2026-08-11 19:26:09 +0300

    Fix gitignore and untrack env

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/upd_todo_db"
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL:  str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- database.py
+++ database.py
@@ -1,20 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped
-from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy.orm import mapped_column
 
-from config import DATABASE_URL
+from config import settings
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=True)
 
 Session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
-
-class User(Base):
-    __tablename__ = "Users"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str]
-    hashed_password: Mapped[str]
 
 
