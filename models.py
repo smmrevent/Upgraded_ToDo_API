0e287c132b48f2913c1e0571344af818084cc247
Daniil Trush <mghts11@gmail.com>
2026-08-12 11:16:21 +0300

    Fix alembic env.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from database import Base
 from sqlalchemy.orm import mapped_column, Mapped
 
 class User(Base):
-    __tablename__ = "Users"
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str]
     hashed_password: Mapped[str]
