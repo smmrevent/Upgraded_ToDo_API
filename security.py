fe9c1920d1479bf05c090feec51dcd3c7ab037ef
Daniil Trush <mghts11@gmail.com>
2026-08-20 03:00:09 +0300

    Updated hash metod for refresh tokens

--- security.py
+++ security.py
@@ -1,15 +1,14 @@
 from passlib.context import CryptContext
+import hashlib
 
 pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
-tkn_context = CryptContext(schemes="SHA256", deprecated="auto")
 
 def hash_password(password: str):
     hashed_password = pwd_context.hash(password)
     return hashed_password
 
 def hash_token(token: str):
-    hashed_token = tkn_context.hash(token)
-    return hashed_token
+    return hashlib.sha256(token.encode('utf-8')).hexdigest()
 
 def verify_password(plain_password,hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
